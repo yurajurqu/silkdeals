00dab0b53119705199663714d01e3edb9438744e
obarboza <obarboza@pe.ibm.com>
2021-03-10 12:44:00 -0500

    first commit

---
+++ silkdeals/spiders/example.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy_selenium import SeleniumRequest
+
+class ExampleSpider(scrapy.Spider):
+    name = 'example'
+
+    def start_requests(self):
+        yield SeleniumRequest(
+            url="https://duckduckgo.com",
+            wait_time=3,
+            screenshot=True,
+            callback=self.parse
+        )
+
+    def parse(self, response):
+        img = response.meta['screenshot']
+
+        with open('screenshot.png', 'wb') as f:
+            f.write(img)
